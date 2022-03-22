5c2918cc08cc40b00e2a1573e4a997baf8d2dc4b
Scott Nichols <s21nichols@hotmail.com>
2022-03-21 22:48:59 -0600

    added some images to about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import aboutImage from "../../assets/cover/body_cover.JPG";
 
 function About() {
   return (
-    <section>
+    <section className='my-5'>
       <h1 id="about">About Me...</h1>
+      <img src={aboutImage} className="my-2" style={{ width: "100%" }} alt="cover" />
     </section>
   );
 }
