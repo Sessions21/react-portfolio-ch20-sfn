3df1ed55a45c145e7c48451a495a9d7eefbe4007
Scott Nichols <s21nichols@hotmail.com>
2022-03-26 21:19:00 -0600

    added resume and about details

--- src/components/Pages/About/indexAbout.js
+++ src/components/Pages/About/indexAbout.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import aboutImage from "../../../assets/cover/about-cover.png";
+import bodyImage from "../../../assets/cover/body_cover.JPG";
+
 
 function About() {
   return (
     <section className='my-5'>
       <h1 id="about">About Me...</h1>
+      <img src={bodyImage} className="img-thumbnail mx-1" alt="person looking off in the distance" />
+      <p>With over 15 years of experience in a range of IT positions, with the majority of time spent as an IBM 
+        Systems Engineer, my exploration into web design pulls from a 
+        breadth of influences.  I have interfaced, and become extremely familiar with, countless complex  
+        applications that run businesses and provide social intractions.
+        My priorities in web design include logical symplicity with robust capabilities. I utilize
+        the logic of a mathmatical mind and astethtics of a love of artcreation in all of my web design and 
+        user interfaces. Ultimately, my websites incompass unique simplicity and high web visibilty.
+        Let me design for you and show you what I mean!</p>
       <img src={aboutImage} className="my-2" style={{ width: "100%" }} alt="desk with computer" />
     </section>
   );
--- src/components/Pages/Resume/indexRes.js
+++ src/components/Pages/Resume/indexRes.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import ImageRes from '../../../assets/images_resume/resume.pdf';
 
 const Resume = () => {
   return (
-    <div>Resume</div>
+    <div>
+      <a href={ImageRes} download="resume.pdf"> Download Resume Here </a>
+      <article>
+        <h2>Resume</h2>
+        <h3>Scott F. Nichols Web Designer</h3>
+        <p> With over 12 years of IT experience immersed in all areas of the complex IBM business
+            model, along with a mathematics and business education, and a breadth of leadership
+            roles, I use my broad knowledgebase to provide innovative ideas and unique solutions.
+            I consistently give a dedicated effort to complete any assigned tasks and always seek
+            out ways to improve the state of the business. The uniqueness of my contributions
+            involves the ability to both logically and creatively find thorough solutions to complex
+            situations. Through maximizing efficiency, and a lead-by-example work ethic, I assist in
+            maximizing the returns of any endeavor.
+        </p>
+        <br/>
+        <h4>Education:</h4>
+        <ul>
+          <li>Full Stack Web Design Bootcamp - University of utah (A)</li>
+          <li>Bachelor of Science in Mathematics - Mars Hill University (Cum Laude)</li>
+          <li>Associates in Business and Finance - Salt Lake Community College (Cum Laude)</li>
+        </ul>
+        <br/>
+        <h4>Skills:</h4>
+        <ul>
+          <li>Full Stack Web Design - HTML, CSS, JavaScript, JQuery, Node.js, MERN web design</li>
+          <li>IBM Certified Systems Engineer - Power Systems, IBM Cloud, IBM Storage, ESS</li>
+          <li>Competition Soccer Coach</li>
+          <li>Public Speaker/Instructor</li>
+        </ul>
+      </article>
+    </div>
   )
 }
 
